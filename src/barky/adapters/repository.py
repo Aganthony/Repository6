a5ada0a626158c4ce41f82bc8263824406d31ee8
Aganthony <aganthony1@buffs.wtamu.edu>
2024-04-24 08:33:29 -0500

    initial commit

---
+++ src/barky/adapters/repository.py
@@ -0,0 +1,54 @@
+# pylint: disable=no-member, no-self-use
+from typing import Set
+import abc
+
+# Update these imports to the correct path where your domain models and Django models are located
+from pms.domain import model as domain_model # type: ignore
+from pms_app import models as django_models # type: ignore
+
+class AbstractRepository(abc.ABC):
+    """
+    An abstract base class for the repository, can be used with any data storage strategy.
+    """
+
+    @abc.abstractmethod
+    def add(self, entity):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def list(self):
+        raise NotImplementedError
+
+
+class DjangoRepository(AbstractRepository):
+    """
+    A repository using the Django ORM as the data storage strategy.
+    """
+
+    def add(self, patient: domain_model.Patient):
+        # Transforms the domain model to Django model and saves it to the database.
+        django_model = django_models.Patient.from_domain(patient)
+        django_model.save()
+
+    def get(self, patient_id) -> domain_model.Patient:
+        django_patient = django_models.Patient.objects.filter(id=patient_id).first()
+        if django_patient:
+            return django_patient.to_domain()
+
+    def list(self):
+        return [
+            patient.to_domain() for patient in django_models.Patient.objects.all()
+        ]
+
+    def update(self, patient: domain_model.Patient):
+        # Update the Django model corresponding to the domain model.
+        django_patient = django_models.Patient.objects.filter(id=patient.id).first()
+        if django_patient:
+            # If you have fields to update, they would be set here
+            # django_patient.name = patient.name, etc.
+            django_patient.save()
+
